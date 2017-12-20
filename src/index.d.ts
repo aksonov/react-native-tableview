013c0fa0789b42a584fbd3ad842244efb25f6251
Kyle Roach <kroach.work@gmail.com>
2017-12-20 09:05:54 -0400

    Remove toplevel module

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,108 +4,125 @@
 // TypeScript Version: 2.6
 
 import * as React from 'react'
+import { ViewStyle } from 'react-native'
 
-declare module 'react-native-tableview' {
-  export interface Constants {
-    Style: {
-      Plain: number
-      Grouped: number
-    }
-    CellStyle: {
-      Default: number
-      Value1: number
-      Value2: number
-      Subtitle: number
-    }
-    CellEditingStyle: {
-      None: number
-      Delete: number
-      Insert: number
-    }
-    CellSelectionStyle: {
-      None: number
-      Blue: number
-      Gray: number
-      Default: number
-    }
-    SeparatorStyle: {
-      None: number
-      Line: number
-    }
-    AccessoryType: {
-      None: number
-      DisclosureIndicator: number
-      DisclosureButton: number
-      Checkmark: number
-      DetailButton: number
-    }
+export interface Constants {
+  Style: {
+    Plain: number
+    Grouped: number
   }
-
-  enum AccessoryType {
-    None = 0,
-    DisclosureIndicator,
-    DisclosureButton,
-    Checkmark,
-    DetailButton
+  CellStyle: {
+    Default: number
+    Value1: number
+    Value2: number
+    Subtitle: number
   }
-
-  interface SectionProps {
-    /**
-     * Show the DisclosureIndicator accessory type
-     */
-    arrow?: boolean
-
-    /**
-     * Title for header
-     */
-    label?: string
-
-    /**
-     * Title for header
-     */
-    footerLabel?: string
-
-    /**
-     * Height of header
-     */
-    headerHeight?: number
-
-    /**
-     * Height of footer
-     */
-    footerHeight?: number
+  CellEditingStyle: {
+    None: number
+    Delete: number
+    Insert: number
   }
-
-  interface ItemProps {
-    /**
-     * This value will be returned on event callbacks
-     */
-    value?: string | number
-
-    /**
-     * Show the Checkmark accessory type
-     */
-    selected?: boolean
-
-    /**
-     * Detail text to show
-     */
-    detail?: string
-
-    /**
-     * Accessory type
-     */
-    accessoryType?: AccessoryType
+  CellSelectionStyle: {
+    None: number
+    Blue: number
+    Gray: number
+    Default: number
   }
-
-  class TableView extends React.Component<{}> {}
-
-  namespace TableView {
-    const Consts: Constants
-    class Section extends React.Component<SectionProps> {}
-    class Item extends React.Component<ItemProps> {}
-    class Cell extends React.Component<ItemProps> {}
+  SeparatorStyle: {
+    None: number
+    Line: number
   }
-
-  export default TableView
+  AccessoryType: {
+    None: number
+    DisclosureIndicator: number
+    DisclosureButton: number
+    Checkmark: number
+    DetailButton: number
+  }
 }
+
+export enum AccessoryType {
+  None = 0,
+  DisclosureIndicator,
+  DisclosureButton,
+  Checkmark,
+  DetailButton,
+}
+
+export enum TableViewStyle {
+  Plain = 0,
+  Grouped,
+}
+
+export enum TableViewCellStyle {
+  Default = 0,
+  Value1,
+  Value2,
+  Subtitle,
+}
+
+interface SectionProps {
+  /**
+   * Show the DisclosureIndicator accessory type
+   */
+  arrow?: boolean
+
+  /**
+   * Title for header
+   */
+  label?: string
+
+  /**
+   * Title for header
+   */
+  footerLabel?: string
+
+  /**
+   * Height of header
+   */
+  headerHeight?: number
+
+  /**
+   * Height of footer
+   */
+  footerHeight?: number
+}
+
+interface ItemProps {
+  /**
+   * This value will be returned on event callbacks
+   */
+  value?: string | number
+
+  /**
+   * Show the Checkmark accessory type
+   */
+  selected?: boolean
+
+  /**
+   * Detail text to show
+   */
+  detail?: string
+
+  /**
+   * Accessory type
+   */
+  accessoryType?: AccessoryType
+}
+
+interface TableViewProps {
+  style?: ViewStyle
+  tableViewStyle?: TableViewStyle
+  tableViewCellStyle?: TableViewCellStyle
+}
+
+declare class TableView extends React.Component<TableViewProps> {}
+
+declare namespace TableView {
+  const Consts: Constants
+  class Section extends React.Component<SectionProps> {}
+  class Item extends React.Component<ItemProps> {}
+  class Cell extends React.Component<ItemProps> {}
+}
+
+export default TableView
