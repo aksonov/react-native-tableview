81f9f87d3f3b41274b0cfc8d2da416a77405805a
Kyle Roach <kroach.work@gmail.com>
2018-05-29 23:46:45 -0400

    Add scrollToIndex example

---
+++ example/src/screens/Example8.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Button, View } from 'react-native'
+import TableView from 'react-native-tableview'
+
+const { Section, Item } = TableView
+
+class Example8 extends React.Component{
+  render() {
+    return(
+      <View style={{flex: 1}}>
+
+        <Button title="Scroll To Section 2" onPress={() => this.tableView.scrollToIndex({index: 2, section: 1})} />
+
+        <TableView
+          style={{ flex: 1 }}
+          allowsToggle
+          ref={ref => this.tableView = ref}
+          allowsMultipleSelection
+          tableViewStyle={TableView.Consts.Style.Grouped}
+          tableViewCellStyle={TableView.Consts.CellStyle.Subtitle}
+          onPress={event => console.log(event)}
+        >
+          <Section label="Section 1" arrow>
+            <Item value="1" detail="Detail1">
+              Item 1
+            </Item>
+            <Item value="2">Item 2</Item>
+            <Item>Item 3</Item>
+          </Section>
+      
+          <Section label="Section 2" arrow={false}>
+            <Item>Item 1</Item>
+            <Item>Item 2</Item>
+            <Item>Item 3</Item>
+          </Section>
+      
+          <Section label="Section 3" arrow={false}>
+            <Item>Item 1</Item>
+            <Item>Item 2</Item>
+            <Item>Item 3</Item>
+            <Item>Item 4</Item>
+            <Item>Item 5</Item>
+            <Item>Item 6</Item>
+            <Item>Item 7</Item>
+            <Item>Item 8</Item>
+            <Item>Item 9</Item>
+            <Item>Item 10</Item>
+          </Section>
+        </TableView>
+      </View>
+    )
+  }
+} 
+
+export default Example8
