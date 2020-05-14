e4132e77c606cb9c958718d268aa63777198b317
Dylan <dylancompanjen@gmail.com>
2020-05-14 14:00:49 +0200

    Update RNTableView.m

--- RNTableView/RNTableView.m
+++ RNTableView/RNTableView.m
@@ -212,8 +212,6 @@ RCT_NOT_IMPLEMENTED(-initWithCoder:(NSCoder *)aDecoder)
     _tableView.allowsMultipleSelectionDuringEditing = NO;
     _tableView.contentInset = self.contentInset;
     _tableView.contentOffset = self.contentOffset;
-    _tableView.separatorInset = UIEdgeInsetsMake(0, 16, 0, 16);
-    _tableView.layoutMargins = UIEdgeInsetsMake(0, 16, 0, 16);
     _tableView.scrollIndicatorInsets = self.scrollIndicatorInsets;
     _tableView.showsHorizontalScrollIndicator = self.showsHorizontalScrollIndicator;
     _tableView.showsVerticalScrollIndicator = self.showsVerticalScrollIndicator;
